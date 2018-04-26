0b6818892b533d78d250408d9d2db99eaafb242c
endless-summer <lylesimpson89@gmail.com>
2018-04-26 17:19:58 +0100

    User model added, and database schema updated

---
+++ src/main/java/wpd2/cw/grouph/milestoneplanner/models/User.java
@@ -0,0 +1,65 @@
+package wpd2.cw.grouph.milestoneplanner.models;
+
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="USER")
+public class User {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue
+    private int id;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="password")
+    private String password;
+
+    @Transient
+    private String passwordConfirm;
+
+    @Column(name="firstname")
+    private String firstname;
+
+    @Column(name="surname")
+    private String surname;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public User() { super(); }
+    //private Set<Role> roles;
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,7 @@ INSERT INTO MILESTONE (title, description, due_date) VALUES ('Create unit tests'
 INSERT INTO MILESTONE (title, description, due_date) VALUES ('Wireframes', 'Craft a beautiful set of wireframes', CURRENT_TIMESTAMP());
 INSERT INTO MILESTONE (title, description, due_date) VALUES ('User Authentication', 'Create a register and login system', CURRENT_TIMESTAMP());
 INSERT INTO MILESTONE (title, description, due_date) VALUES ('Database relationships', 'Associate a user with the milestones they create', CURRENT_TIMESTAMP());
+
+INSERT INTO User (username, password, firstname, surname) VALUES ('lyle', 'lyle1234', 'Lyle', 'Simpson');
+INSERT INTO User (username, password, firstname, surname) VALUES ('dave', 'dave1234', 'Dave', 'Sumpster');
+INSERT INTO User (username, password, firstname, surname) VALUES ('abdul', 'abdul1234', 'Abdul', 'Dar');
