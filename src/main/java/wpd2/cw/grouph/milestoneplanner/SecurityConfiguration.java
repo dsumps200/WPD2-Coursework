397f39bb7df3bf01e6a9997f0dc665d3e28d0586
endless-summer <lylesimpson89@gmail.com>
2018-04-26 16:56:18 +0100

    Header/footer, login and register Mustache files added. Authentication config set up

---
+++ src/main/java/wpd2/cw/grouph/milestoneplanner/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package wpd2.cw.grouph.milestoneplanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        /* Allow access to the root URL ("/"), but force authentication for others */
+        http.authorizeRequests().antMatchers("/", "/register", "/css/*", "/js/*", "/img/*").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .and()
+            .logout()
+            .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("pw")
+                .roles("USER");
+    }
+}
+
---
+++ src/main/java/wpd2/cw/grouph/milestoneplanner/UserController.java
@@ -0,0 +1,21 @@
+package wpd2.cw.grouph.milestoneplanner;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class UserController {
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage() {
+        return "register";
+    }
+
+}
