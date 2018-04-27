75fa4f0de0f0168472e3bef367df4f3b18ea40cb
endless-summer <lylesimpson89@gmail.com>
2018-04-27 17:14:51 +0100

    JavaScript validation functions added, for validating add-milestone form

--- src/main/resources/static/js/validators.js
+++ src/main/resources/static/js/validators.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
     /* Add Milestone validators */
+    $("#create-milestone-form").on("submit", function(e) {
+        // Validate each field in the form - min/max length, format (for dates), etc
+        var title = $("#title");
+        var description = $("#description");
+        var intendedDueDate = $("#intended-date");
 
+        /* Milestone title */
+        if (title.val().length < 4 || title.val().length > 64) {
+            e.preventDefault();
+            title.next().text("Title must be between 4 and 64 characters")
+        }
+
+        // Clear the title error when user types
+        title.on("keydown", function() {
+            title.next().empty()
+        })
+
+    })
+
+    /* Register form validators */
 });
