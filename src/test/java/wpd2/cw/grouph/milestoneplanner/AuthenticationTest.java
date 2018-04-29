c82f9fc2b64f6df290b271e4f174f55cdb6e5802
endless-summer <lylesimpson89@gmail.com>
2018-04-29 12:16:41 +0100

    Added test to ensure admin page not accessible

--- src/test/java/wpd2/cw/grouph/milestoneplanner/AuthenticationTest.java
+++ src/test/java/wpd2/cw/grouph/milestoneplanner/AuthenticationTest.java
@@ -1,30 +1,44 @@
 package wpd2.cw.grouph.milestoneplanner;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.net.URL;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class MilestoneControllerTest {
+public class AuthenticationTest {
 
     @Autowired
     private MockMvc mvc;
 
+    private String base = "http://localhost/";
+
+
     @Test
-    public void getHello() throws Exception {
+    public void getIndex() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
+
+    /* Tests to ensure that, by default, access to the admin page is prevented, and redirects to login */
+    @Test
+    public void preventAccessToAdmin() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/admin"))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl(this.base + "login"));
     }
 }
--- src/test/java/wpd2/cw/grouph/milestoneplanner/MilestonePlannerApplicationTests.java
+++ src/test/java/wpd2/cw/grouph/milestoneplanner/MilestonePlannerApplicationTests.java
@@ -1,18 +1,18 @@
 // package wpd2.cw.grouph.milestoneplanner;
-
+//
 // import org.junit.Test;
 // import org.junit.runner.RunWith;
 // import org.springframework.boot.test.context.SpringBootTest;
 // import org.springframework.test.context.junit4.SpringRunner;
 // import org.springframework.util.Assert;
-
+//
 // @RunWith(SpringRunner.class)
 // @SpringBootTest
 // public class MilestonePlannerApplicationTests {
-
+//
 // 	@Test
 // 	public void contextLoads() {
 // 		Assert.isTrue(true);
 // 	}
-
+//
 // }
